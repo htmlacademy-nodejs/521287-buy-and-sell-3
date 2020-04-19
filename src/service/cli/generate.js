3c8df6c7ee6d5686f63962c95bd1abcd621df2db
Ilya Sidorchik <ilya@sidorchik.ru>
2020-04-19 22:19:55 +0300

    Rewrite callbacks with async/await in generate

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const fs = require(`fs`);
+const fs = require(`fs`).promises;
 const chalk = require(`chalk`);
 
 const {getRandomInt, shuffle, getPictureFileName} = require(`../../utils`);
@@ -10,7 +10,7 @@ const {
   CATEGORIES,
   OFFER_TYPE,
   SUM_RESTRICT,
-  PICTURE_RESTRICT
+  PICTURE_RESTRICT,
 } = require(`../../data.js`);
 
 const DEFAULT_COUNT = 1;
@@ -23,31 +23,28 @@ const generateOffers = (count) => {
     .fill({})
     .map(() => ({
       category: [CATEGORIES[getRandomInt(0, CATEGORIES.length - 1)]],
-      description: shuffle(SENTENCES)
-        .slice(1, 5)
-        .join(` `),
+      description: shuffle(SENTENCES).slice(1, 5).join(` `),
       picture: getPictureFileName(
           getRandomInt(PICTURE_RESTRICT.min, PICTURE_RESTRICT.max)
       ),
       title: TITLES[getRandomInt(0, TITLES.length - 1)],
       type: TYPE[getRandomInt(0, TYPE.length - 1)],
-      sum: getRandomInt(SUM_RESTRICT.min, SUM_RESTRICT.max)
+      sum: getRandomInt(SUM_RESTRICT.min, SUM_RESTRICT.max),
     }));
 };
 
 module.exports = {
   name: `--generate`,
-  run(args) {
+  async run(args) {
     const [count] = args;
     const countOffer = Number.parseInt(count, 10) || DEFAULT_COUNT;
     const content = JSON.stringify(generateOffers(countOffer), null, 2);
 
-    fs.writeFile(FILE_NAME, content, (err) => {
-      if (err) {
-        return console.error(chalk.red(`Can't write data to file...`));
-      }
-
-      return console.log(chalk.green(`Operation success. File created.`));
-    });
-  }
+    try {
+      await fs.writeFile(FILE_NAME, content);
+      console.log(chalk.green(`Operation success. File created.`));
+    } catch (err) {
+      console.error(chalk.red(`Can't write data to file...`));
+    }
+  },
 };
