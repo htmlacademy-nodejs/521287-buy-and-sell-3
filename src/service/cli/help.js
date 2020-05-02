ae20e2bdbc04940e327e0463ad449c1c141959d3
Ilya Sidorchik <ilya@sidorchik.ru>
2020-05-02 20:29:39 +0300

    Add server command to help

--- src/service/cli/help.js
+++ src/service/cli/help.js
@@ -5,15 +5,16 @@ const chalk = require(`chalk`);
 module.exports = {
   name: `--help`,
   run() {
-    const text = `Программа запускает http-сервер и формирует файл с данными для АПИ.
+    const text = `Программа запускает ХТПП-сервер и формирует файл с данными для АПИ.
 
     Гайд:
     server <command>
 
     Команды:
-      --version             выводит номер версии
-      --help                печатает этот текст
-      --generate <count>    формирует файл mocks.json
+    --version             выводит номер версии
+    --help                печатает этот текст
+    --generate <count>    формирует файл mocks.json
+    --server <port>       запускает ХТТП-сервер
     `;
 
     console.info(chalk.gray(text));
