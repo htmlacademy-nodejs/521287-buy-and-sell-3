248eb4d1dca666f1db40b4171d53cbccd0091bbf
Ilya Sidorchik <ilya@sidorchik.ru>
2020-05-02 19:44:59 +0300

    Make server response of mocks titles

--- src/constants.js
+++ src/constants.js
@@ -6,3 +6,10 @@ module.exports.ExitCode = {
   SUCCESS: 0,
   ERROR: 1,
 };
+module.exports.HttpCode = {
+  OK: 200,
+  UNAUTHORIZED: 401,
+  FORBIDDEN: 403,
+  NOT_FOUND: 404,
+  INTERNAL_SERVER_ERROR: 500,
+};
