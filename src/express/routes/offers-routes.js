df2ddf7891718e1d9707d255e17f26a4cea1f516
Ilya Sidorchik <ilya@sidorchik.ru>
2020-05-24 21:11:47 +0300

    Render offers

--- src/express/routes/offers-routes.js
+++ src/express/routes/offers-routes.js
@@ -1,13 +1,13 @@
-"use strict";
+'use strict';
 
 const {Router} = require(`express`);
 
 const offersRouter = new Router();
-const ROOT = `/offers`;
+const ROOT = `offers`;
 
-offersRouter.get(`/category/:id`, (req, res) => res.render(`offers/category`));
-offersRouter.get(`/add`, (req, res) => res.render(`offers/add`));
-offersRouter.get(`/edit/:id`, (req, res) => res.render(`offers/edit`));
-offersRouter.get(`/:id`, (req, res) => res.send(`${ROOT}${req.url}`));
+offersRouter.get(`/category/:id`, (req, res) => res.render(`${ROOT}/category`));
+offersRouter.get(`/add`, (req, res) => res.render(`${ROOT}/add`));
+offersRouter.get(`/edit/:id`, (req, res) => res.render(`${ROOT}/edit`));
+offersRouter.get(`/:id`, (req, res) => res.render(`${ROOT}/offer`));
 
 module.exports = offersRouter;
