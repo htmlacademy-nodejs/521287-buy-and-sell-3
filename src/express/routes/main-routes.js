ae62863b7f6d6a5bd4635fedeb13f4f1b72c1e0e
Ilya Sidorchik <ilya@sidorchik.ru>
2020-05-24 20:21:12 +0300

    Render search

--- src/express/routes/main-routes.js
+++ src/express/routes/main-routes.js
@@ -7,6 +7,6 @@ const mainRouter = new Router();
 mainRouter.get(`/`, (req, res) => res.send(`/`));
 mainRouter.get(`/sign-up`, (req, res) => res.render(`sign-up`));
 mainRouter.get(`/login`, (req, res) => res.render(`login`));
-mainRouter.get(`/search`, (req, res) => res.send(`/search`));
+mainRouter.get(`/search`, (req, res) => res.render(`search`));
 
 module.exports = mainRouter;
