1fafade349bc0250deb8383fb66ef20d0050da18
Ilya Sidorchik <ilya@sidorchik.ru>
2020-05-24 20:44:18 +0300

    Render comments

--- src/express/routes/my-routes.js
+++ src/express/routes/my-routes.js
@@ -6,6 +6,6 @@ const myRouter = new Router();
 const ROOT = `/my`;
 
 myRouter.get(`/`, (req, res) => res.send(ROOT));
-myRouter.get(`/comments`, (req, res) => res.send(`${ROOT}/comments`));
+myRouter.get(`/comments`, (req, res) => res.render(`my/comments`));
 
 module.exports = myRouter;
