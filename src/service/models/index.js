9585263c3575963148cec59616cdb3aa7de40a4c
Ilya Sidorchik <ilya@sidorchik.ru>
2021-01-17 16:43:52 +0300

    6.2 Add database sequelize models

---
+++ src/service/models/index.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const {Model} = require(`sequelize`);
+
+const Aliase = require(`./aliase`);
+const defineCategory = require(`./category`);
+const defineComment = require(`./comment`);
+const defineOffer = require(`./offer`);
+
+class OfferCategory extends Model {}
+
+const define = (sequelize) => {
+  const Category = defineCategory(sequelize);
+  const Comment = defineComment(sequelize);
+  const Offer = defineOffer(sequelize);
+
+  Offer.hasMany(Comment, {as: Aliase.COMMENTS, foreignKey: `offerId`});
+  Comment.belongsTo(Offer, {foreignKey: `offerId`});
+
+  OfferCategory.init({}, {sequelize});
+
+  Offer.belongsToMany(Category, {through: OfferCategory, as: Aliase.CATEGORIES});
+  Category.belongsToMany(Offer, {through: OfferCategory, as: Aliase.OFFERS});
+  Category.hasMany(OfferCategory, {as: Aliase.OFFER_CATEGORIES});
+
+  return {Category, Comment, Offer, OfferCategory};
+};
+
+module.exports = define;
