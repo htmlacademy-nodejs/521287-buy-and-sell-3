58997df1213cc23ce6f1ef0555d9e84a6a7f566b
Ilya Sidorchik <ilya@sidorchik.ru>
2021-07-31 21:17:45 +0300

    7.3 Add protection from CSRF attacks

--- src/express/routes/offers-routes.js
+++ src/express/routes/offers-routes.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const {Router} = require(`express`);
+const csrf = require(`csurf`);
 
 const {buildOfferData} = require(`../../utils`);
 const {checkAuth, upload} = require(`../middlewares`);
@@ -9,38 +10,50 @@ const api = require(`../api`).getAPI();
 const ROOT = `offers`;
 
 const offersRouter = new Router();
+const csrfProtection = csrf();
 
-offersRouter.get(`/add`, checkAuth, async (req, res) => {
+offersRouter.get(`/add`, checkAuth, csrfProtection, async (req, res) => {
   const {user} = req.session;
   const {error} = req.query;
   const categories = await api.getCategories();
+  const csrfToken = req.csrfToken();
 
   res.render(`${ROOT}/add`, {
     user,
     categories,
     error,
+    csrfToken,
   });
 });
 
-offersRouter.post(`/add`, checkAuth, upload.single(`avatar`), async (req, res) => {
-  const offerData = buildOfferData(req);
+offersRouter.post(
+    `/add`,
+    checkAuth,
+    upload.single(`avatar`),
+    csrfProtection,
+    async (req, res) => {
+      const offerData = buildOfferData(req);
 
-  try {
-    await api.createOffer(offerData);
+      try {
+        await api.createOffer(offerData);
 
-    return res.redirect(`../my`);
-  } catch (error) {
-    const errorMessage = encodeURIComponent(error.response && error.response.data);
-    const errorPath = `/offers/add?error=${errorMessage}`;
+        return res.redirect(`../my`);
+      } catch (error) {
+        const errorMessage = encodeURIComponent(
+            error.response && error.response.data
+        );
+        const errorPath = `/offers/add?error=${errorMessage}`;
 
-    return res.redirect(errorPath);
-  }
-});
+        return res.redirect(errorPath);
+      }
+    }
+);
 
-offersRouter.get(`/edit/:id`, checkAuth, async (req, res) => {
+offersRouter.get(`/edit/:id`, checkAuth, csrfProtection, async (req, res) => {
   const {user} = req.session;
   const {id} = req.params;
   const {error} = req.query;
+  const csrfToken = req.csrfToken();
 
   const [offer, categories] = await Promise.all([
     api.getOffer(id),
@@ -53,30 +66,40 @@ offersRouter.get(`/edit/:id`, checkAuth, async (req, res) => {
     offer,
     categories,
     error,
+    csrfToken,
   });
 });
 
-offersRouter.post(`/edit/:id`, checkAuth, upload.single(`avatar`), async (req, res) => {
-  const {id} = req.params;
+offersRouter.post(
+    `/edit/:id`,
+    checkAuth,
+    upload.single(`avatar`),
+    csrfProtection,
+    async (req, res) => {
+      const {id} = req.params;
 
-  const offerData = buildOfferData(req, req.body[`old-image`]);
+      const offerData = buildOfferData(req, req.body[`old-image`]);
 
-  try {
-    await api.editOffer(id, offerData);
+      try {
+        await api.editOffer(id, offerData);
 
-    return res.redirect(`/my`);
-  } catch (error) {
-    const errorMessage = encodeURIComponent(error.response && error.response.data);
-    const errorPath = `/offers/edit/${id}/?error=${errorMessage}`;
+        return res.redirect(`/my`);
+      } catch (error) {
+        const errorMessage = encodeURIComponent(
+            error.response && error.response.data
+        );
+        const errorPath = `/offers/edit/${id}/?error=${errorMessage}`;
 
-    return res.redirect(errorPath);
-  }
-});
+        return res.redirect(errorPath);
+      }
+    }
+);
 
-offersRouter.get(`/:id`, async (req, res) => {
+offersRouter.get(`/:id`, csrfProtection, async (req, res) => {
   const {user} = req.session;
   const {id} = req.params;
   const {error} = req.query;
+  const csrfToken = req.csrfToken();
 
   const offer = await api.getOffer(id, true);
   const author = offer ? offer.users : {};
@@ -86,29 +109,37 @@ offersRouter.get(`/:id`, async (req, res) => {
     id,
     pugOffer: offer,
     author,
-    error
+    error,
+    csrfToken,
   });
 });
 
-offersRouter.post(`/:id/comments`, checkAuth, async (req, res) => {
-  const {user} = req.session;
-  const {id} = req.params;
-  const {comment} = req.body;
+offersRouter.post(
+    `/:id/comments`,
+    checkAuth,
+    csrfProtection,
+    async (req, res) => {
+      const {user} = req.session;
+      const {id} = req.params;
+      const {comment} = req.body;
 
-  try {
-    await api.createComment(id, {
-      userId: user.id,
-      text: comment,
-    });
+      try {
+        await api.createComment(id, {
+          userId: user.id,
+          text: comment,
+        });
 
-    return res.redirect(`../offers/${id}`);
-  } catch (error) {
-    const errorMessage = encodeURIComponent(error.response && error.response.data);
-    const errorPath = `/offers/${id}/?error=${errorMessage}`;
+        return res.redirect(`../offers/${id}`);
+      } catch (error) {
+        const errorMessage = encodeURIComponent(
+            error.response && error.response.data
+        );
+        const errorPath = `/offers/${id}/?error=${errorMessage}`;
 
-    return res.redirect(errorPath);
-  }
-});
+        return res.redirect(errorPath);
+      }
+    }
+);
 
 offersRouter.get(`/category/:id`, async (req, res) => {
   const {user} = req.session;
